855a597582b56bdf3061395a65f571bf20bcc2ac
Jon Richter <jon@allmende.io>
2024-04-10 05:13:34 +0200

    🏵

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-rm -rf ./.flatpak-builder ./build-dir ./package package-lock.json generated-source.json
+rm -rf ./.flatpak-builder ./build-dir ./package package-lock.json generated-sources.json
